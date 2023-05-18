daa4238583229cf920687a56238369e9995a890e
Nathan Gartside <gar18014@byui.edu>
2023-05-18 12:40:52 -0600

    Added an error message for missing product

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -4,15 +4,19 @@ import { setLocalStorage } from './utils.mjs';
 export async function productDetails(productID, selector) {
     // use findProductById to get the details for the current product. findProductById will return a promise! use await or .then() to process it
     var product = await findProductById(productID);
-    // once we have the product details we can render out the HTML
     console.log(product);
-    document.querySelector('#productName').innerHTML = product.Name;
-    document.querySelector('#productNameWithoutBrand').innerHTML = product.NameWithoutBrand;
-    document.querySelector('#productImage').setAttribute('src', product.Image);
-    document.querySelector('#productFinalPrice').innerHTML = product.FinalPrice;
-    document.querySelector('#productDescriptionHtmlSimple').innerHTML = product.DescriptionHtmlSimple;
-    document.querySelector('#addToCart').setAttribute('data-id', productID);
-    
+    if (product === undefined) {
+        let section = document.querySelector('.product-detail');
+        section.innerHTML = '<h2>This product does not exist</h2>';
+    } else {
+        // once we have the product details we can render out the HTML
+        document.querySelector('#productName').innerHTML = product.Name;
+        document.querySelector('#productNameWithoutBrand').innerHTML = product.NameWithoutBrand;
+        document.querySelector('#productImage').setAttribute('src', product.Image);
+        document.querySelector('#productFinalPrice').innerHTML = product.FinalPrice;
+        document.querySelector('#productDescriptionHtmlSimple').innerHTML = product.DescriptionHtmlSimple;
+        document.querySelector('#addToCart').setAttribute('data-id', productID);
+    }
 
     // add a listener to Add to Cart button
 }
